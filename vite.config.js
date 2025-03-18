02b91675ee72ef86bc81e55ff8857f1a51a83599
Yash Singh <myemail>
2025-03-18 12:43:40 +0530

    vaibhav cam

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    fs: {
+      // Allow serving files from one level up to the project root
+      allow: ['..']
+    }
+  }
 })
